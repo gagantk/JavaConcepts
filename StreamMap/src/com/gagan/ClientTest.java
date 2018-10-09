55ed4185a56f098bea957ca8dfbd6d7f0764b39b
gagantk <gagantk07@gmail.com>
2018-10-09 17:45:19 +0530

    Streams

--- StreamMap/src/com/gagan/ClientTest.java
+++ StreamMap/src/com/gagan/ClientTest.java
@@ -1,10 +1,22 @@
 package com.gagan;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class ClientTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		List<String> list = new ArrayList<>();
+		list.add("C:\\MyFile1.txt");
+		list.add("D:\\MyFile2.txt");
+		list.add("C:\\MyFile3.txt");
+		
+		Stream<Path> map = list.stream().map(uri -> Paths.get(uri));
+		map.forEach(System.out::println);
 	}
 
 }
